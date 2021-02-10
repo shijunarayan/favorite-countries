2e7225bd539eb0ec2a4e03f87e4c2f14f9f0fbdd
shiju narayan <shijunarayan@gmail.com>
2021-02-09 21:55:50 -0500

    updated configs

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -1,16 +1,16 @@
-let apiUrls = {
+const apiUrls = {
   supplementalCountriesUrl: 'https://admithub-suppl-countries.herokuapp.com/supplemental/countries/',
   restcountriesUrl: 'https://restcountries.eu/rest/v2/name/',
   countriesUrl: 'https://admithub-suppl-countries.herokuapp.com/pinned/countries/',
 }
 
-if (window.location.hostname === 'localhost') {
-  console.log('Urls are using dev environment settings');
-  apiUrls = {
-    supplementalCountriesUrl: 'http://127.0.0.1:8000/supplemental/countries/',
-    restcountriesUrl: 'https://restcountries.eu/rest/v2/name/',
-    countriesUrl: 'http://127.0.0.1:8000/pinned/countries/',
-  }
-}
+// if (window.location.hostname === 'localhost') {
+//   console.log('Urls are using dev environment settings');
+//   apiUrls = {
+//     supplementalCountriesUrl: 'http://127.0.0.1:8000/supplemental/countries/',
+//     restcountriesUrl: 'https://restcountries.eu/rest/v2/name/',
+//     countriesUrl: 'http://127.0.0.1:8000/pinned/countries/',
+//   }
+// }
 
 export default apiUrls
